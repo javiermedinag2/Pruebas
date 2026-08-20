1b592172c55ee59f5e50f2fd4dc645343088e2a5
Javier Medina <javiermedinag@hotmail.com>
2026-08-19 20:17:52 -0600

    Visto en clase con 8°O

---
+++ main.py
@@ -0,0 +1,11 @@
+def clima(temperatura):
+    if temperatura < 0:
+        return "Helando"
+    elif 0 <= temperatura < 10:
+        return "Frio"
+    elif 10 <= temperatura < 20:
+        return "Fresco"
+    elif 20 <= temperatura < 30:
+        return "Caluroso"
+    else:
+        return "Caliente"
---
+++ test-main.py
@@ -0,0 +1,12 @@
+from main import clima
+
+def test_clima():
+    assert clima(50) == "Helando"
+def test_clima_frio():
+    assert clima(5) == "Frio"
+def test_clima_fresco():
+    assert clima(15) == "Fresco"
+def test_clima_caluroso():
+    assert clima(300) == "Caluroso"
+def test_clima_caliente():
+    assert clima(35) == "Caliente"

203782f01271a103f4bf6c4d252c9043f3c4ca32
Javier Medina <javiermedinag@hotmail.com>
2026-08-24 11:41:13 -0600

    Fallos intencionados

--- test-main.py
+++ test-main.py
@@ -3,10 +3,10 @@ from main import clima
 def test_clima():
     assert clima(50) == "Helando"
 def test_clima_frio():
-    assert clima(5) == "Frio"
+    assert clima(50) == "Frio"
 def test_clima_fresco():
     assert clima(15) == "Fresco"
 def test_clima_caluroso():
-    assert clima(300) == "Caluroso"
+    assert clima(25) == "Caluroso"
 def test_clima_caliente():
     assert clima(35) == "Caliente"
